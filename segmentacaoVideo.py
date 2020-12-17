19b6135d9bc1c5f440bd5a1d90d84355d1f9ff6d
viniciusTei <viniciustprates@gmail.com>
2020-12-17 10:27:08 -0300

    Analiste ataque ou defesa

--- segmentacaoVideo.py
+++ segmentacaoVideo.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 from centroidTracker import CentroidTracker
-
+from collections import OrderedDict
+from scipy.spatial import distance
 def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -64,14 +65,45 @@ def getContours(img, image, cor):
 
     return rects
 
+def calculaDistancia(initialDict, currentDict):
+    dist = OrderedDict()
+
+    for key, value in initialDict.items():
+        for current_key, current_value in currentDict.items():
+            if key == current_key:
+                dist[key] = value[0] - current_value[0]
+
+    return dist
+
+def defineAtaqueOuDefesa(distancias):
+    values = distancias.values()
+    soma = 0
+    for value in values:
+        soma += value
+    
+    if soma > 0:
+        result = 'ATACANDO'
+    else:
+        result = 'DEFENDENDO'
+    
+    return result
+
+    
 def main():
     ct = CentroidTracker()
     #Abre o vídeo gravado em disco
     camera = cv2.VideoCapture('run.mp4')
-
+    first_object = OrderedDict()
+    distancias = OrderedDict()
+    firstFrame = True
+    count_frames = 0
+    result = 'Nothing'
     while True:
         #read() retorna 1-Se houve sucesso e 2-O próprio frame
         (sucesso, frame) = camera.read()
+        
+        count_frames += 1
+        
         if not sucesso: #final do vídeo
             #recomeca o video
             camera = cv2.VideoCapture('run.mp4')
@@ -83,18 +115,31 @@ def main():
         lower_blue = np.array([70,150,150])
         upper_blue = np.array([155,255,255])
 
-        # lower_red = np.array([159,0,0])
-        # upper_red = np.array([255,255,255])
+        lower_red = np.array([159,0,0])
+        upper_red = np.array([255,255,255])
 
         #mascara encontrada usando a funcao inRange
         #mask_red = findColor(image, lower_red, upper_red)
         mask_blue = findColor(image, lower_blue, upper_blue)
         
         #desenha contornos na imagem
-        bboxes_blue = getContours(mask_blue, image, (255,0,0))
+        bboxes = getContours(mask_blue, image, (0,0,255))
 
-        new_objects = ct.update(bboxes_blue)
+        new_objects = ct.update(bboxes)
         #loop over the tracked objects
+        if firstFrame:
+            first_object = new_objects.copy()
+            firstFrame = False
+        else:
+            distancias = calculaDistancia(first_object, new_objects)
+            if count_frames == 30:
+                #analiza ataque ou defesa
+                result = defineAtaqueOuDefesa(distancias)
+                count_frames = 0
+
+        #escreve na tela o resultado
+        cv2.putText(image, result, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
         for (objectID, centroid) in new_objects.items():
             # draw both the ID of the object and the centroid of the
             # object on the output frame            
@@ -108,9 +153,9 @@ def main():
         #res_red = cv2.bitwise_and(image,image, mask= mask_blue)                
 
         img_stacked = stackImages(0.4, ([frame,mask_blue], [image,res_blue]))
-        cv2.imshow("Exibindo video: Time azul", img_stacked)
+        #cv2.imshow("Exibindo video: Time azul", img_stacked)
         
-        #cv2.imshow("Result", image)
+        cv2.imshow("Result", image)
         key = cv2.waitKey(1) & 0xFF
 
         #Espera que a tecla 'q' seja pressionada para sair
